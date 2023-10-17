b8c72bcdc064a1db10c2eaa1205cccfad3e0d040
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-10-17 02:59:49 -0600

    Add: Base Typescript Projeect

---
+++ dist/bin/server.js
@@ -0,0 +1,55 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_js_1 = __importDefault(require("../app.js"));
+const debug_1 = __importDefault(require("debug"));
+const http_1 = __importDefault(require("http"));
+let port = normalizePort(process.env.PORT || '3000');
+app_js_1.default.set('port', port);
+let server = http_1.default.createServer(app_js_1.default);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + (addr === null || addr === void 0 ? void 0 : addr.port);
+    (0, debug_1.default)('Listening on ' + bind);
+}
+exports.default = server;
